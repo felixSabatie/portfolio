f0d73ff939fac43013cd9d051c776ae94bc5c057
Unknown <felix.sabatie@gmail.com>
2018-12-27 16:57:40 -0500

    Init all components

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="portfolio">
-    <h1>Hello portfolio</h1>
+    <Header></Header>
+    <WelcomeText></WelcomeText>
+    <About></About>
+    <Projects></Projects>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-  export default {
+  import Header from './Header'
+  import WelcomeText from './WelcomeText'
+  import About from './About'
+  import Projects from './Projects'
+  import Footer from './Footer'
 
+  export default {
+    components: { Header, WelcomeText, About, Projects, Footer }
   }
 </script>
---
+++ src/components/WelcomeText.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="welcome-text">
+    <h1>Welcome Text</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+
+</style>
