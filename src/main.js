2aef76c7af8e4ee6b7e849605667a8a0a0cb1662
Unknown <felix.sabatie@gmail.com>
2018-12-27 17:22:47 -0500

    Vue lang integration

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+import VueI18n from 'vue-i18n'
+
 library.add(faCoffee)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(VueI18n)
 
 Vue.config.productionTip = false
 
