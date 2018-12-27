a3cacdb678f58a92f0cff686ab4c6d367f2d0f1c
Unknown <felix.sabatie@gmail.com>
2018-12-27 18:38:34 -0500

    Welcome text style done

--- src/components/WelcomeText.vue
+++ src/components/WelcomeText.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="welcome-text">
-    <h1>{{ $t('welcomeText.title') }}</h1>
-    <div v-html="$t('welcomeText.text')"></div>
+  <div class="welcome-text container">
+    <h1 class="title">{{ $t('welcomeText.title') }}</h1>
+    <div class="text" v-html="$t('welcomeText.text')"></div>
   </div>
 </template>
 
@@ -12,5 +12,23 @@
 </script>
 
 <style lang="scss">
+  @import '../styles/colors';
 
+  .welcome-text {
+    text-align: center;
+    color: $contrastPrimaryColor;
+    letter-spacing: 1.5px;
+
+    .title {
+      margin-top: 100px;
+      margin-bottom: 70px;
+      letter-spacing: 2px;
+    }
+
+    .text {
+      max-width: 500px;
+      font-size: 20px;
+      margin: auto auto 100px;
+    }
+  }
 </style>
