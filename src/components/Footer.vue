71fe512627b35d6cce3d8ed5e8f8ac5a6d6bea90
Unknown <felix.sabatie@gmail.com>
2018-12-27 22:42:04 -0500

    Website github link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,8 @@
   <div class="footer">
     <img class="logo" src="../assets/logo.svg" alt="Félix Sabatié">
     <p class="copyright">Copyright © 2019 Félix Sabatié</p>
+    <p class="repository">{{ $t('footer.footer') }}</p>
+    <p><a class="icon" href="https://github.com/felixSabatie/portfolio" target="_blank"><font-awesome-icon :icon="['fab', 'github']" /></a></p>
   </div>
 </template>
 
@@ -18,13 +20,20 @@
     text-align: center;
     color: $contrastPrimaryColor;
     padding: 80px 0;
+    font-size: 15px;
 
     .logo {
       width: 40px;
       margin-bottom: 20px;
     }
     .copyright {
-      font-size: 15px;
+    }
+    .repository {
+      margin: 10px 0;
+    }
+    .icon {
+      font-size: 30px;
+      color: $contrastPrimaryColor;
     }
   }
 </style>
--- src/lang/fr/index.js
+++ src/lang/fr/index.js
@@ -1,7 +1,8 @@
 import welcomeText from './welcome-text'
 import about from './abount'
 import projects from './projects'
+import footer from './footer'
 
 export default {
-  welcomeText, about, projects
+  welcomeText, about, projects, footer
 }
